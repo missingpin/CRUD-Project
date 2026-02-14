209df37c6ab4b2646e02e86f10a848b341be69e9
missingpin <bunyi.carlandrei@gmail.com>
2026-02-14 13:46:30 +0800

    Add files via upload

---
+++ CRUD Project/action.php
@@ -0,0 +1,30 @@
+<?php
+$con = mysqli_connect("localhost","root","","informationdb");
+
+if(!$con){
+    die('Connection Failed'. mysqli_connect_error());
+}
+if(isset($_POST['delete_data']))
+    {
+        $data_id = mysqli_real_escape_string($con, $_POST['data_id']);
+        $query = "DELETE FROM userinfo WHERE id='$data_id'";
+        $query_run =mysqli_query($con,$query);
+
+        if($query_run){
+            $res = [
+                'status' => 200,
+                'message' => 'Deleted Successfully'
+            ];
+            echo json_encode($res);
+            return;
+        }
+        else{
+            $res = [
+                'status'=> 500,
+                'message'=> 'Not Deleted'
+            ];
+        echo json_encode($res);
+        return;
+    }
+}
+?>
