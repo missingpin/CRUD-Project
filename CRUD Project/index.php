7e4372c2fef1dd4c32136b858838f20dd7d8ae4a
missingpin <bunyi.carlandrei@gmail.com>
2026-02-14 15:03:13 +0800

    Add files via upload

--- CRUD Project/index.php
+++ CRUD Project/index.php
@@ -9,6 +9,12 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 </head>
 <body>
+    <?php
+        include './insert-modal.php';
+    ?>
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    Launch demo modal
+    </button>
     <table id="table-information" class="table table-striped">
         <thead>
             <tr>
@@ -46,5 +52,6 @@
         </tbody>
     </table>
     <script src="script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
